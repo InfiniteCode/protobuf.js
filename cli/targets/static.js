11e66532d1f7e65834a6307ea79919ab96e3d158
alex <InfiniteCode@gmail.com>
2018-01-15 11:36:19 +1100

    added test comment

--- cli/targets/static.js
+++ cli/targets/static.js
@@ -662,7 +662,7 @@ function buildService(ref, service) {
         ]);
         push(escapeName(service.name) + ".prototype" + util.safeProp(lcName) + " = function " + escapeName(lcName) + "(request, callback) {");
             ++indent;
-            push((config.es6 ? "const" : "var") + " method = " + escapeName(service.name) + ".CLASS_TYPE + \"/" + method.name + "\"";);
+            push((config.es6 ? "const" : "var") + " method = " + escapeName(service.name) + ".CLASS_TYPE + \"" + method.name + "\";");
             push("return this.rpcCall(" + escapeName(lcName) + ", $root." + exportName(method.resolvedRequestType) + ", $root." + exportName(method.resolvedResponseType) + ", request, callback);");
             --indent;
         push("};");
